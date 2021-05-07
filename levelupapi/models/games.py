8deef682ab3fef01e4f397fee0d5e8e37b1f9e11
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-05-07 14:26:39 -0500

    Ch 7 finished

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -1,3 +1,4 @@
+from django.db.models.aggregates import Max
 from levelupapi.models.game_type import GameType
 from django.db import models
 from django.contrib.auth.models import User
@@ -9,3 +10,5 @@ class Game(models.Model):
     name_of_game = models.CharField(max_length=50)
     skill_level = models.CharField(max_length=50)
     how_many_players = models.CharField(max_length=50)
+    maker = models.CharField(max_length=50)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
