9f32d7d4914ebb43fa976d9c908df93d008cb14f
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-05-07 16:11:19 -0500

    Started chapter 8

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -5,7 +5,8 @@ from django.db.models.deletion import CASCADE
 class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    datetime = models.DateTimeField()
+    date =  models.TimeField(default="")
+    time = models.DateField(default="")
     description = models.CharField(max_length=50)
     number_of_people = models.CharField(max_length=50)
     attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="Attending")
