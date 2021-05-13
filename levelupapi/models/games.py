8ee8bebcd869c3712563f0b26dcc61e6e25c268e
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-05-13 14:15:22 -0500

    finished debugging games/new

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 
 class Game(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type_of_game = models.ForeignKey(GameType, on_delete=models.CASCADE)
     name_of_game = models.CharField(max_length=50)
     skill_level = models.CharField(max_length=50)
-    how_many_players = models.CharField(max_length=50)
+    how_many_players = models.TextField()
     maker = models.CharField(max_length=50)
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
